78db714aa1275ff656d2707509717d51a72940f7
MOHAMMED NAWAS  MARYAM <maryamnawas03@gmail.com>
2025-10-25 10:32:16 +0530

    feat: initialize React frontend with TypeScript and Tailwind CSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from "react";
+import TaskList from "./components/TaskList";
+
+function App() {
+  return (
+    <div className="App">
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/TaskList.tsx
@@ -0,0 +1,15 @@
+// src/components/TaskList.tsx
+import React from "react";
+
+const TaskList = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-semibold">Task List</h2>
+      <ul>
+        {/* Tasks will be rendered here */}
+      </ul>
+    </div>
+  );
+};
+
+export default TaskList;
